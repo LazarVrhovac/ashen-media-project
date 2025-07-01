269ab0c386c20eae126af594269cdd36de887a63
Lazar Vrhovac <lazarvrhovac.work@gmail.com>
2025-07-01 18:46:00 +0200

    rootLayout and bars

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 
 import RootLayout from './_root/RootLayout';
 import SignInForm from './_auth/forms/SignInForm';
-import { Home } from './_root/pages';
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from './_root/pages';
 import SignUpForm from './_auth/forms/SignUpForm';
 import AuthFormLayout from './_auth/AuthFormLayout';
 import './globals.css';
@@ -13,15 +23,23 @@ const App = () => {
   return (
     <main className='flex h-screen'>
       <Routes>
-        {/* public routes */}
+        {/* public */}
         <Route element={<AuthFormLayout />}>
           <Route path='/sign-in' element={<SignInForm />} />
           <Route path='/sign-up' element={<SignUpForm />} />
         </Route>
 
-        {/* private routes */}
+        {/* private */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path='/explore' element={<Explore />} />
+          <Route path='/saved' element={<Saved />} />
+          <Route path='/all-users' element={<AllUsers />} />
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/update-post/:id' element={<EditPost />} />
+          <Route path='/posts/:id' element={<PostDetails />} />
+          <Route path='/profile/:id/*' element={<Profile />} />
+          <Route path='/update-profile/:id' element={<UpdateProfile />} />
         </Route>
       </Routes>
 
--- src/lib/tanstack-query/queriesAndMutations.ts
+++ src/lib/tanstack-query/queriesAndMutations.ts
@@ -4,7 +4,11 @@ import {
   useQueryClient,
   useInfiniteQuery,
 } from '@tanstack/react-query';
-import { createNewUserAcc, signInAccount } from '../appwrite/api';
+import {
+  createNewUserAcc,
+  signInAccount,
+  signOutAccount,
+} from '../appwrite/api';
 import type { INewUser } from '@/types';
 
 export const useCreateUserAccount = () => {
@@ -17,5 +21,11 @@ export const useSignInAccount = () => {
   return useMutation({
     mutationFn: (user: { email: string; password: string }) =>
       signInAccount(user),
+  });
+};
+
+export const useSignOutInAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount,
   });
 };
